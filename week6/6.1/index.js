24fd764ad2446cc15c583fa4531771d5a5a14bad
Aman Fangeria <aman.fangeria@geekyants.com>
2024-09-16 15:59:42 +0530

    JWT token created, sign and verify done

--- week6/6.1/index.js
+++ week6/6.1/index.js
@@ -1,81 +1,10 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
 const users = [];
-
-function generateToken() {
-    let options = [
-        "a",
-        "b",
-        "c",
-        "d",
-        "e",
-        "f",
-        "g",
-        "h",
-        "i",
-        "j",
-        "k",
-        "l",
-        "m",
-        "n",
-        "o",
-        "p",
-        "q",
-        "r",
-        "s",
-        "t",
-        "u",
-        "v",
-        "w",
-        "x",
-        "y",
-        "z",
-        "A",
-        "B",
-        "C",
-        "D",
-        "E",
-        "F",
-        "G",
-        "H",
-        "I",
-        "J",
-        "K",
-        "L",
-        "M",
-        "N",
-        "O",
-        "P",
-        "Q",
-        "R",
-        "S",
-        "T",
-        "U",
-        "V",
-        "W",
-        "X",
-        "Y",
-        "Z",
-        "0",
-        "1",
-        "2",
-        "3",
-        "4",
-        "5",
-        "6",
-        "7",
-        "8",
-        "9",
-    ];
-
-    let token = "";
-    for (let i = 0; i < 32; i++) {
-        // use a simple function here
-        token += options[Math.floor(Math.random() * options.length)];
-    }
-    return token;
-}
-
+dotenv.config();
+const secret = process.env.JWT_SECRET;
 const app = express();
 app.use(express.json());
 app.post("/signup", (req, res) => {
@@ -107,11 +36,11 @@ app.post("/signin", (req, res) => {
         else return false;
     });
     if (foundUser) {
-        const token = generateToken();
-        foundUser.token = token;
+        // const token = generateToken();
+        const token = jwt.sign({ username: foundUser.username }, secret);
         console.log(users);
         return res.json({
-            message: token,
+            token: token,
         });
     } else
         return res.json({
@@ -120,9 +49,13 @@ app.post("/signin", (req, res) => {
 });
 
 app.get("/me", (req, res) => {
-    const token = req.headers.authorization;
+    const token = req.headers.authorization; //jwt token
     console.log(token);
-    const user = users.find((u) => u.token === token);
+    const decodedInformation = jwt.verify(token, secret); // {username: "amanfang"}
+    console.log(decodedInformation);
+    const username = decodedInformation.username;
+
+    const user = users.find((u) => u.username === username);
     if (user)
         res.send({
             username: user.username,
